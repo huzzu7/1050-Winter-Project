c21cbe43f89e8f8fbb2745b2752d93a1fc36b496
huzaifa <84243533+huzzu7@users.noreply.github.com>
2025-03-25 15:25:09 -0400

    Update 1050.ino

--- src/1050.ino
+++ src/1050.ino
@@ -1,3 +1,4 @@
+
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -8,75 +9,87 @@
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1
 #define SCREEN_ADDRESS 0x3C
-#define FAN_PIN 3
+#define FAN_PIN 10
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 SparkFun_ENS160 myENS;
 Adafruit_AHTX0 aht;
 
 void setup() {
-  Serial.begin(9600);
-  Wire.begin();
+    Serial.begin(9600);
+    Wire.begin();
+    pinMode(FAN_PIN, OUTPUT);
 
-  pinMode(ForwardPin, OUTPUT); 
+    // Initialize Display
+    if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+        Serial.println("Display not detected!");
+        while (1);
+    }
+    display.setRotation(0);
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.display();
+    delay(1000);
 
-  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println("Display not detected!");
-    while (1);
-  }
-  display.setRotation(1); 
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.display();
-  delay(1000);
+    if (!myENS.begin() || !aht.begin()) {
+        Serial.println("Sensor Error!");
+    }
 
-  if (!myENS.begin() || !aht.begin()) {
-    Serial.println("Sensor Error!");
-  }
-
-  myENS.setOperatingMode(SFE_ENS160_RESET);
-  delay(100);
-  myENS.setOperatingMode(SFE_ENS160_STANDARD);
+    myENS.setOperatingMode(SFE_ENS160_RESET);
+    delay(100);
+    myENS.setOperatingMode(SFE_ENS160_STANDARD);
 }
 
 void loop() {
-  if (myENS.checkDataStatus()) {
-    int AQI = myENS.getAQI();
-    int TVOC = myENS.getTVOC();
-    int eCO2 = myENS.getECO2();
-    sensors_event_t humidity, temp;
-    aht.getEvent(&humidity, &temp);
+    if (myENS.checkDataStatus()) {
+      int TVOC = myENS.getTVOC();
+      int eCO2 = myENS.getECO2();
+      int AQI = myENS.getAQI();
+      sensors_event_t humidity, temp;
+      aht.getEvent(&humidity, &temp);
 
-    display.clearDisplay();
-    
-    display.setCursor(0, 0);
-    display.print("AQI:  "); display.println(AQI);
-    display.setCursor(0, 16);
-    display.print("TVOC: "); display.print(TVOC); display.println("ppb");
-    display.setCursor(0, 32);
-    display.print("eCO2: "); display.print(eCO2); display.println("ppm");
+      Serial.print("TVOC: "); Serial.println(TVOC);
+      Serial.print("eCO2: "); Serial.println(eCO2);
+      Serial.print("Temperature: "); Serial.println(temp.temperature);
+      Serial.print("Humidity: "); Serial.println(humidity.relative_humidity);
+      Serial.println("----------------------");
 
-    display.setCursor(0, 56);
-    display.print("Temp: "); display.print(temp.temperature, 1); display.println("C");
-    display.setCursor(0, 72);
-    display.print("RH:   "); display.print(humidity.relative_humidity, 1); display.println("%");
-    
-    display.display();
-  }
-  delay(2000);
+      //Maybe make the logic continuous than discrete?
+      int fanSpeed = 0;
+      if (eCO2 > 1200 || TVOC > 500) {
+          fanSpeed = 255; 
+      } else if (eCO2 > 800 || TVOC > 300) {
+          fanSpeed = 180;
+      } else {
+          fanSpeed = 100;
+      }
+
+      analogWrite(FAN_PIN, fanSpeed);
+
+      display.clearDisplay();
+
+      display.setCursor(0, 0);
+      display.print("TVOC   AQI  eCO2");
+
+      display.setCursor(0, 10);
+      display.print(TVOC); display.print("ppb  ");
+      display.print(AQI); display.print("    ");
+      display.print(eCO2); display.print("ppm");
+
+      display.setCursor(0, 30);
+      display.print("Temp   RH     Fan");
+
+      display.setCursor(0, 40);
+      display.print(temp.temperature, 1); display.print("C  ");
+      display.print(humidity.relative_humidity, 1); display.print("%  ");
+      int displayFanSpeed = (fanSpeed * 100) / 255;
+
+      display.print(displayFanSpeed);display.print("%");
+
+      display.display();
+
+    }
+    delay(200);  
 }
 
-void displayError(const char *message) {
-  display.clearDisplay();
-  display.setCursor(10, 28);
-  display.println(message);
-  display.display();
-  while (1);
-}
-
-void enableFan(void){
-  // if(condition){
-  //   digitalWrite(FAN_PIN, HIGH);
-  // }
-}
